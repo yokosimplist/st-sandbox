be0cc7364ad607c814a07427674a59c74c959abc
Yoko Ishioka <yoko@simplist.com>
2023-12-20 16:02:25 -0500

    Signals demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Signal, WritableSignal, computed, signal } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'st-sandbox';
+
+  count: WritableSignal<number> = signal(0);
+  message: Signal<string> = computed(() => `the count is: ` + this.count())
+
+  count$ = new BehaviorSubject<number>(0);
+
+  addSignal() {
+    this.count.update(value => value + 1);
+    console.log(this.count())
+  }
+
+  addBehaviorSubject() {
+    const value: number = this.count$.value + 1;
+    this.count$.next(value);
+    console.log(this.count())
+  }
 }
